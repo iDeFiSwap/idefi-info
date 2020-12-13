5ae6b0f74d9c0a661d73f2c09be9c3153a35ea44
henry <hoang.nong.nv@gmail.com>
2020-12-13 15:00:35 +0000

    Change logo, title to Idefi

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 import { Flex } from 'rebass'
 import Link from '../Link'
 import { RowFixed } from '../Row'
-import Logo from '../../assets/logo_white.svg'
+import Logo from '../../assets/logo.png'
 import Wordmark from '../../assets/wordmark_white.svg'
 
 const TitleWrapper = styled.div`
@@ -35,7 +35,7 @@ export default function Title() {
           <UniIcon id="link" onClick={() => history.push('/')}>
             <img width={'24px'} src={Logo} alt="logo" />
           </UniIcon>
-          <img width={'84px'} style={{ marginLeft: '8px', marginTop: '0px' }} src={Wordmark} alt="logo" />
+          <h1 style={{ marginLeft: '8px', marginTop: '18px' }}>IDEFI</h1>
         </RowFixed>
       </Flex>
     </TitleWrapper>
